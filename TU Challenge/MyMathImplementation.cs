e100795b672dcd4d0edfa8ac69896c16c69cfd5b
JoshuaLetessier <jletessier@gaming.tech>
2024-04-29 22:24:13 +0200

    Ajoutez des fichiers projet.

---
+++ TU Challenge/MyMathImplementation.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TU_Challenge
+{
+    public class MyMathImplementation
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static List<int> GenericSort(List<int> toSort, Func<int, int, int> isInOrder)
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        public static int IsInOrder(int a, int b)
+        {
+            if (a == b)
+                return 0;
+            if (a > b)
+                return -1;
+            else
+                return 1;
+        }
+
+        public static int IsInOrderDesc(int arg1, int arg2)
+        {
+            if(arg1 == arg2)
+                return 0;
+            if(arg1 < arg2) 
+                return -1;
+            else
+                return 1;
+        }
+
+        public static bool IsListInOrder(List<int> list)
+        {
+            int lastInt = 0;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if(i == 0)
+                {
+                    lastInt = list[i];
+                }
+                else
+                {
+                    if(IsInOrder(lastInt, list[i]) == 1 || IsInOrder(lastInt, list[i]) == 0)
+                    {
+                        lastInt = list[i];
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public static List<int> Sort(List<int> toSort)
+        {
+            List<int> list = new List<int>();
+            int lastInt = 0;
+            int actualInt = 0;
+
+            int sizeList = list.Count;
+            int sizeToSort = toSort.Count;
+            int indexList =0;
+
+            for (int y = 0; y < toSort.Count; y++)
+            {
+                if(toSort.Count == 1)
+                {
+                    actualInt = toSort[y];
+                }
+                else
+                {
+                    for (int i = 0; i < toSort.Count; i++)
+                    {
+                        if (i == 0)
+                        {
+                            lastInt = toSort[i];
+                            actualInt = lastInt;
+                        }
+                        else
+                        {
+                            if (IsInOrder(lastInt, toSort[i]) == -1)
+                            {
+                                actualInt = toSort[i];
+                                indexList = i;
+                                lastInt = toSort[i];
+                            }
+                        }
+                    }
+                }
+                
+                list.Add(actualInt);
+                if (toSort.Remove(actualInt) == false)
+                    new Exception("pas sup");
+                actualInt = 0;
+                indexList = 0;
+            }
+            return list;
+        }
+
+        public static List<int> GetAllPrimary(int a)
+        {
+            List<int> list = new List<int>();
+            for (int i = 2; i <= a; i++)
+            {
+                if (IsPrimary(i) == true)
+                {
+                    list.Add(i);
+                }
+            }
+            return list;
+        }
+
+        public static bool IsDivisible(int a, int b)
+        {
+            if (a % b == 0 && b > 0)
+                return true;
+            return false;
+        }
+
+        public static bool IsEven(int a)
+        {
+            if (a % 2 == 0)
+                return true;
+            return false;
+        }
+
+        public static bool IsMajeur(int age)
+        {
+            if (age >= 18 && age > 0)
+                return true;
+            else if (age < 0)
+                throw new ArgumentException("Valeur d'age négative");
+            else if (age > 140)
+                throw new ArgumentException("Valeur d'age trop grande vous êtes mort");
+            else
+                return false;
+        }
+
+        public static bool IsPrimary(int a)
+        {
+            for(int i = 2; i < a; i++)
+            {
+                if (IsDivisible(a, i) == true)
+                    return false;
+            }
+            return true;
+        }
+
+        public static int Power(int a, int b)
+        {
+            return (int)Math.Pow(a, b);
+        }
+
+        public static int Power2(int a)
+        {
+            return (int)Math.Pow(a, 2);
+        }
+
+
+    }
+}
---
+++ TU Challenge/MyStringImplementation.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TU_Challenge
+{
+    public class MyStringImplementation
+    {
+        public static string BazardString(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsNullEmptyOrWhiteSpace(string input)
+        {
+            if (input == null || input.Length == 0)
+                return true;
+            else
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    
+                    if (input[i] != ' ')
+                        return false;
+                }
+                return true;
+            }
+        }
+
+        public static string MixString(string a, string b)
+        {
+            string result = "";
+            if (a == null || a.Length == 0)
+                return b;
+            if (b == null || b.Length == 0)
+                return a;
+            if(a.Length < b.Length)
+            {
+                for (int i = 0; i < b.Length; i++)
+                {
+                    if (i == a.Length)
+                        return result + b.Substring(i).ToString();
+                    else if (i == b.Length)
+                        return result + a.Substring(i).ToString();
+                    else
+                        result = result + a[i].ToString() + b[i].ToString();
+                }
+            }
+            else
+            {
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (i == a.Length)
+                        return result + b.Substring(i).ToString();
+                    else if (i == b.Length)
+                        return result + a.Substring(i).ToString();
+                    else
+                        result = result + a[i].ToString() + b[i].ToString();
+                }
+            }
+           
+            return result;
+
+        }
+
+        public static string Reverse(string a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ToCesarCode(string input, int offset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ToLowerCase(string a)
+        {
+            string result = "";
+            
+            for(int i = 0; i < a.Length; i++)
+            {
+                switch(a[i])
+                {
+                    case 'É':
+                        result = result + 'é';
+                        break;
+                    case 'À':
+                        result = result + 'à';
+                        break;
+                    case 'È':
+                        result = result + 'è';
+                        break;
+                }
+                if (a[i]  >= 'A' && a[i]<= 'Z' && result.Length != a.Length)
+                    result = result + (char)(a[i] + 32);
+                else if(result.Length != a.Length)
+                    result = result + a[i];
+            }
+            return result;
+            throw new NotImplementedException();
+        }
+
+        public static string UnBazardString(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Voyelles(string a)
+        {
+            a = ToLowerCase(a);
+            for (int i = 0;i < a.Length;i++)
+            {
+               // if (a[i]. || a[i] = 'e')
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
